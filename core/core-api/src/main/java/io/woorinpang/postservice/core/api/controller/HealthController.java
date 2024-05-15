c5accc7050b1a2069de78244d5da045ce8ab3fa8
heechul <heechul4296@gmail.com>
2024-05-15 16:02:37 +0900

    feature : #10 - post append test 추가

--- core/core-api/src/main/java/io/woorinpang/postservice/core/api/controller/HealthController.java
+++ core/core-api/src/main/java/io/woorinpang/postservice/core/api/controller/HealthController.java
@@ -9,6 +9,6 @@ public class HealthController {
 
     @GetMapping("/health")
     public ResponseEntity<Object> health() {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body("post-service");
     }
 }
